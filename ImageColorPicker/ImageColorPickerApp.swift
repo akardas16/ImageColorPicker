86f000eda5745a183874d1d8bf4f66a336ef8b0e
akardas16 <akardas16@ku.edu.tr>
2022-10-03 16:51:56 +0300

    Initial Commit

---
+++ ImageColorPicker/ImageColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageColorPickerApp.swift
+//  ImageColorPicker
+//
+//  Created by Abdullah Kardas on 3.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
